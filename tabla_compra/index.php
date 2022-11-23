85a05aa7c749eefc370ec0a11b683d02a313d9ed
Manuel <53manuelrodriguez@gmail.com>
2022-11-23 17:29:27 -0500

    cuarto commit

--- tabla_compra/index.php
+++ tabla_compra/index.php
@@ -9,17 +9,22 @@ $conexion = mysqli_connect('localhost','root','','autos');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
-    <table border="1">
+<div class="container">
+        <h1 class="text-center" style="background-color: black; color:white">LISTADO DE AUTOS</h1>
+    </div>
+    <div class="container">
+    <table class="table">
         <tr>
-            <td>Fecha_venta</td>
-            <td>Marca</td>
-            <td>Modelo</td>
-            <td>Cantidad_venta</td>
-            <td>precio_unidad</td>
-            <td>Total_venta</td>
+            <td scope="col">Fecha_venta</td>
+            <td scope="col">Marca</td>
+            <td scope="col">Modelo</td>
+            <td scope="col">Total_venta</td>
+            <td scope="col">precio_unidad</td>
+            <td scope="col">Cantidad venta</td>
         </tr>
         <?php
         $sql = "SELECT * FROM modelo m INNER JOIN marca mc INNER JOIN ventas v ON m.fk_marca = mc.id_marca AND v.fk_modelo = m.id_modelo";
